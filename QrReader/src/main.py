b83a80f4674bec760280fd19970c57f57acdb431
Jorbis21 <javierorbis@gmail.com>
2026-08-06 21:59:32 +0200

    Cambio de audios y gitignore

--- QrReader/src/main.py
+++ QrReader/src/main.py
@@ -1,7 +1,7 @@
 import os, sys
 from PyQt6.QtWidgets import QApplication
+from dotenv import load_dotenv
 
-# Importa tus servicios y controladores
 from services.audio import AudioService
 from models.ai_manager import AIManager
 from models.serial_manager import SerialMonitor
@@ -10,11 +10,16 @@ from models.vision import VisionEngine
 from models.translator import MicrobitCompiler
 from models.voice_control import VoiceCommandManager
 from controllers.app_controller import AppController
-from controllers.camera_controller import CameraController
-from controllers.json_controller import JsonController
-from controllers.qr_controller import QRController
+
 
 def main():
+    load_dotenv()
+    
+    api_key = os.getenv("GEMINI_API_KEY", "")
+    
+    if not api_key:
+        print("ADVERTENCIA: No se ha encontrado GEMINI_API_KEY en el archivo .env")
+
     base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
     workspace_dir = os.path.join(base_dir, 'workspace')
     config_dir = os.path.join(base_dir, 'data', 'config')
@@ -25,22 +30,17 @@ def main():
 
     app = QApplication(sys.argv)
 
-    # 1. INSTANCIAR SERVICIOS BASE
     audio_service = AudioService()
-    audio_service.iniciar() # Arrancamos el hilo de voz de forma controlada
+    audio_service.iniciar()
 
-    # 2. INSTANCIAR MODELOS Y MOTORES (Inyectando el audio donde haga falta)
     voice_manager = VoiceCommandManager(None, workspace_dir, audio_service)
     vision_engine = VisionEngine()
     json_manager = JsonManager(config_dir)
     traductor = MicrobitCompiler(config_dir, json_manager)
     
-    # Fíjate cómo ahora le pasamos el audio por parámetro
-    ai_manager = AIManager("AQ.Ab8RN6JQTC-SYK-S--HwCZ1vUbUvZ6-z-Frek--H-vkNUdFJ-w", audio_service) 
+    ai_manager = AIManager(api_key, audio_service) 
     serial_monitor = SerialMonitor(audio_service)
 
-    # 3. INSTANCIAR EL CONTROLADOR PRINCIPAL
-    # Le pasamos todo ya fabricado para que no tenga que usar 'new'
     controlador_principal = AppController(
         workspace_dir, assets_dir, audio_service, vision_engine, 
         json_manager, traductor, ai_manager, serial_monitor, voice_manager
